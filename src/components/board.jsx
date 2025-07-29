a4469845c6b151ec7b198cb6eeba81882c370d35
MattAsm <67015529+MattAsm@users.noreply.github.com>
2025-07-29 11:55:03 -0400

    Update board.jsx

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -45,51 +45,41 @@ function Board(){
         getAllCards();
     }, [])
 
-   //   IN PROCESS OF BEING ABLE TO FLIP CARDS
-   //   next step is to add a useEffect with a setInterval 
-   //   inside to give players a second to memorize the cards before unflipping
-
-   //   All the code between here and the long comment line is chatGPT code. Im studying it and 
-   //   will try to replicate it without looking at it
-   //   This is merely a test to see if the code works, so that i can review and learn from it
     useEffect(() => {
-        if (firstPick && secondPick) {
+       if(firstPick && secondPick){
         setDisabled(true);
 
-        // Check match
-        if (firstPick.pkmnName === secondPick.pkmnName) {
-            setCardArr(prev =>
-            prev.map(card => {
-                if (card.pkmnName === firstPick.pkmnName) {
-                return { ...card, isMatched: true };
-                }
-                return card;
-            })
-            );
-            resetTurn();
+        if(firstPick.pkmnName === secondPick.pkmnName){
+        setCardArr(prev => prev.map(card => {
+           if(card.pkmnName === firstPick.pkmnName){
+            return {...card, isMatched: true};
+           }
+           else
+            return card;
+        }));
+        resetTurn();
         } else {
-            setTimeout(() => {
-            setCardArr(prev =>
-                prev.map(card => {
-                if (card.id === firstPick.id || card.id === secondPick.id) {
-                    return { ...card, isFlipped: false };
+        setTimeout(() => {
+            setCardArr(prev => prev.map(card => {
+                if(card.id === firstPick.id || card.id === secondPick.id){
+                    return {...card, isFlipped: false};
                 }
                 return card;
-                })
-            );
+            }));
             resetTurn();
-            }, 1000); // delay before flipping back
-        }
-        }
+        }, 1000);
+      }
+    }
   }, [firstPick, secondPick]);
    
+
     function handleCardClick(clickedCard){
         if(disabled || clickedCard.isFlipped || clickedCard.isMatched){
             return;
         }
 
         const flippedCard = {...clickedCard, isFlipped: true};
-        setCardArr(prev => prev.map(card => (card.id === clickedCard.id ? flippedCard : card)));
+        setCardArr(prev => prev.map(card => card.id === clickedCard.id ? flippedCard : card));
 
         if(!firstPick){
             setFirstPick(flippedCard);
@@ -103,7 +93,7 @@ function Board(){
     setSecondPick(null);
     setDisabled(false);
   }
-//////////////////////////////////////////////////////////////////////////
+
     return(<div id='cardBoard'>
 
         {cardArr.map(card => (
